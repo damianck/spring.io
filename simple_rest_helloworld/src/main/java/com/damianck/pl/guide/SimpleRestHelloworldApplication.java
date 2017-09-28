7259a77b537cb2f944c3b218ecb77c98d8fa5619
Damian Wtorek <damian.wtr@gmail.com>
2017-09-28 15:54:26 +0200

    simple rest - hello world

---
+++ simple_rest_helloworld/src/main/java/com/damianck/pl/guide/SimpleRestHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.damianck.pl.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestHelloworldApplication.class, args);
+	}
+}
